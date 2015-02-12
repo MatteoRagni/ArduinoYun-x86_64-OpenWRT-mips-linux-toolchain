67392c5fc49e0faf69e15967c0947c9d3c3e4f1b
Dipartimento Ingegneria Industriale - Meccatronica <michelangelo@armbuilder.dii.unitn.it>
2015-02-12 09:41:38 +0100

    First commit

---
+++ toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2/initial/lib/gcc/mips-openwrt-linux-uclibc/4.6.4/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/nirvana1289/data/workspace/toolchain/linino/trunk/build_dir/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2/gcc-linaro-4.6-2012.12/configure --prefix=/home/nirvana1289/data/workspace/toolchain/linino/trunk/staging_dir/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2 --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=mips-openwrt-linux-uclibc --with-gnu-ld --enable-target-optspace --disable-libgomp --disable-libmudflap --disable-multilib --disable-nls --with-host-libstdcxx=-lstdc++ --with-float=soft --with-gmp=/home/nirvana1289/data/workspace/toolchain/linino/trunk/staging_dir/host --with-mpfr=/home/nirvana1289/data/workspace/toolchain/linino/trunk/staging_dir/host --disable-decimal-float --with-mips-plt --with-mpc=/home/nirvana1289/data/workspace/toolchain/linino/trunk/staging_dir/host --disable-libssp --disable-__cxa_atexit --with-newlib --with-sysroot=/home/nirvana1289/data/workspace/toolchain/linino/trunk/staging_dir/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2 --enable-languages=c --disable-shared --disable-threads";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "float", "soft" }, { "llsc", "llsc" }, { "mips-plt", "plt" }, { "synci", "no-synci" } };
